f05a93a6f084fd97cb1db8867abe23b544cd9db3
parkso00 <namona2005@naver.com>
2025-03-29 19:33:50 +0900

    기존 에셋추가

---
+++ Assets/Dungeons&Castles/Readme.txt
@@ -0,0 +1,8 @@
+Thanks for buying our product!
+
+For instruction on how to use the package please see :
+https://youtu.be/iEt6Txg8qFM
+
+
+For any question contact us at :
+ motuproprio3000@gmail.com
---
+++ Assets/Dungeons&Castles/script/ActivateDestructible.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateDestructible : MonoBehaviour {
+    
+    private Rigidbody[] myRG;
+	// Use this for initialization
+	void Start () {
+
+        myRG = GetComponentsInChildren<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        { //Bottone Sinistro
+            for (int x = 0; x < myRG.Length; x++)
+            {
+                myRG[x].isKinematic = false;
+            }
+        }
+
+    }
+}
